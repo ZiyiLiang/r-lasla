de0bdd44fa422be5b8907dfa92c893ef3cbda5d5
ZiyiLiang <ziyilian@usc.edu>
2023-08-03 12:27:10 -0700

    Improved graphics.

--- experiments/make_plot.R
+++ experiments/make_plot.R
@@ -1,6 +1,8 @@
 library(tidyverse)
 library(kableExtra)
 
+options(width=160)
+
 #######################################
 #          Plots for paper            #
 #######################################
@@ -16,12 +18,16 @@ Method.labels <- c("PV.OR", "LASLA.OR", "LAWS.OR")
 color.scale <- c("#3366CC", "#66CCFF", "#CC79A7", "orange", "red")
 shape.scale <- c(15, 2, 4, 1, 18)
 
+
 plot_width = 10
 plot_height = 3
-legend_size = 13
-font_size = 16
+legend_size = 14
+font_size = 17
 axis_size = 15
-
+pt_size = 2
+pt_alpha = 0.8
+l_alpha = 0.95
+l_size = 0.7
 
 #######################################
 #       Asymmetric setting            #
@@ -36,25 +42,29 @@ pp <- results  %>%
       pivot_longer(cols=c("FDR", "Power"), names_to='Key', values_to='Value') %>%
       mutate(Key = factor(Key, key.values, key.labels))  %>%
 ggplot(aes(x=Gamma, y=Value, color=Method, shape=Method)) +
-geom_point(alpha=0.75) +
-geom_line() +
+  geom_point(alpha=pt_alpha, size=pt_size) +
+  geom_line(size=l_size, alpha=l_alpha) +
 geom_hline(data=df.nominal, aes(yintercept=Value)) +
 # sets the axis limit
 geom_point(data=df.ghost, aes(x=1,y=0.03), alpha=0) +
 geom_point(data=df.ghost, aes(x=1,y=0.07), alpha=0) +
 scale_color_manual(values=color.scale) +
 scale_shape_manual(values=shape.scale) +
-theme_light()+
+scale_alpha_manual(values=alpha.scale) +
+theme_bw()+
 theme(
   strip.text = element_text(size = font_size, color = "black"),
   axis.title = element_text(size = font_size),
   axis.text = element_text(size = axis_size),
   legend.text = element_text(size = legend_size),
-  legend.title = element_text(size = legend_size)
+  legend.title = element_text(size = legend_size),
+  plot.title = element_text(hjust = -0.11, vjust = -4, size=font_size),
 )+
 facet_wrap(.~Key, scales="free") +
 xlab(expression(gamma)) +
-ylab("")
+ylab("")+
+ggtitle("(a)")
+pp
 
 ggsave(filename = sprintf("%s/asymmetric.pdf", fig.dir), plot = pp,
        dpi = 300, device=NULL, width=plot_width, height=plot_height)
@@ -74,25 +84,29 @@ pp <- results  %>%
   pivot_longer(cols=c("FDR", "Power"), names_to='Key', values_to='Value') %>%
   mutate(Key = factor(Key, key.values, key.labels))  %>%
   ggplot(aes(x=Sigma, y=Value, color=Method, shape=Method)) +
-  geom_point(alpha=0.75) +
-  geom_line() +
+  geom_point(alpha=pt_alpha, size=pt_size) +
+  geom_line(size=l_size, alpha=l_alpha) +
   geom_hline(data=df.nominal, aes(yintercept=Value)) +
   # sets the axis limit
   geom_point(data=df.ghost, aes(x=1,y=0.03), alpha=0) +
   geom_point(data=df.ghost, aes(x=1,y=0.07), alpha=0) +
   scale_color_manual(values=color.scale) +
   scale_shape_manual(values=shape.scale) +
-  theme_light()+
+  theme_bw()+
   theme(
     strip.text = element_text(size = font_size, color = "black"),
     axis.title = element_text(size = font_size),
     axis.text = element_text(size = axis_size),
     legend.text = element_text(size = legend_size),
-    legend.title = element_text(size = legend_size)
+    legend.title = element_text(size = legend_size),
+    plot.title = element_text(hjust = -0.11, vjust = -4, size=font_size),
+    
   )+
   facet_wrap(.~Key, scales="free") +
   xlab(expression(sigma)) +
-  ylab("")
+  ylab("")+
+  ggtitle("(b)")
+
 
 
 ggsave(filename = sprintf("%s/alt_dist.pdf", fig.dir), plot = pp,
@@ -116,25 +130,27 @@ pp <- results  %>%
   pivot_longer(cols=c("FDR", "Power"), names_to='Key', values_to='Value') %>%
   mutate(Key = factor(Key, key.values, key.labels))  %>%
   ggplot(aes(x=Mean, y=Value, color=Method, shape=Method)) +
-  geom_point(alpha=0.75) +
-  geom_line() +
+  geom_point(alpha=pt_alpha, size=pt_size) +
+  geom_line(size=l_size, alpha=l_alpha) +
   geom_hline(data=df.nominal, aes(yintercept=Value)) +
   # sets the axis limit
   geom_point(data=df.ghost, aes(x=2.5,y=0.1), alpha=0) +
   geom_point(data=df.ghost, aes(x=2.5,y=0), alpha=0) +
   scale_color_manual(values=color.scale) +
   scale_shape_manual(values=shape.scale) +
-  theme_light()+
+  theme_bw()+
   theme(
     strip.text = element_text(size = font_size, color = "black"),
     axis.title = element_text(size = font_size),
     axis.text = element_text(size = axis_size),
     legend.text = element_text(size = legend_size),
-    legend.title = element_text(size = legend_size)
+    legend.title = element_text(size = legend_size),
+    plot.title = element_text(hjust = -0.11, vjust = -4, size=font_size),
   )+
   facet_wrap(.~Key, scales="free") +
   xlab(expression(mu[1])) +
-  ylab("")
+  ylab("")+
+  ggtitle("(a)")
 
 
 ggsave(filename = sprintf("%s/network1.pdf", fig.dir), plot = pp,
@@ -158,25 +174,28 @@ pp <- results  %>%
   pivot_longer(cols=c("FDR", "Power"), names_to='Key', values_to='Value') %>%
   mutate(Key = factor(Key, key.values, key.labels))  %>%
   ggplot(aes(x=Distance, y=Value, color=Method, shape=Method)) +
-  geom_point(alpha=0.75) +
-  geom_line() +
+  geom_point(alpha=pt_alpha, size=pt_size) +
+  geom_line(size=l_size, alpha=l_alpha) +
   geom_hline(data=df.nominal, aes(yintercept=Value)) +
   # sets the axis limit
   geom_point(data=df.ghost, aes(x=0,y=0.1), alpha=0) +
   geom_point(data=df.ghost, aes(x=1,y=0), alpha=0) +
   scale_color_manual(values=color.scale) +
   scale_shape_manual(values=shape.scale) +
-  theme_light()+
+  theme_bw()+
   theme(
     strip.text = element_text(size = font_size, color = "black"),
     axis.title = element_text(size = font_size),
     axis.text = element_text(size = axis_size),
     legend.text = element_text(size = legend_size),
-    legend.title = element_text(size = legend_size)
+    legend.title = element_text(size = legend_size),
+    plot.title = element_text(hjust = -0.11, vjust = -4, size=font_size),
+    
   )+
   facet_wrap(.~Key, scales="free") +
   xlab(expression(mu[2])) +
-  ylab("")
+  ylab("")+
+  ggtitle("(b)")
 
 
 ggsave(filename = sprintf("%s/network2.pdf", fig.dir), plot = pp,
@@ -202,25 +221,27 @@ pp <- results  %>%
   pivot_longer(cols=c("FDR", "Power"), names_to='Key', values_to='Value') %>%
   mutate(Key = factor(Key, key.values, key.labels))  %>%
   ggplot(aes(x=Sigma, y=Value, color=Method, shape=Method)) +
-  geom_point(alpha=0.75) +
-  geom_line() +
+  geom_point(alpha=pt_alpha, size=pt_size) +
+  geom_line(size=l_size, alpha=l_alpha) +
   geom_hline(data=df.nominal, aes(yintercept=Value)) +
   # sets the axis limit
   geom_point(data=df.ghost, aes(x=0.5,y=0.1), alpha=0) +
   geom_point(data=df.ghost, aes(x=2.0,y=0), alpha=0) +
   scale_color_manual(values=color.scale) +
   scale_shape_manual(values=shape.scale) +
-  theme_light()+
+  theme_bw()+
   theme(
     strip.text = element_text(size = font_size, color = "black"),
     axis.title = element_text(size = font_size),
     axis.text = element_text(size = axis_size),
     legend.text = element_text(size = legend_size),
-    legend.title = element_text(size = legend_size)
+    legend.title = element_text(size = legend_size),
+    plot.title = element_text(hjust = -0.11, vjust = -4, size=font_size),
   )+
   facet_wrap(.~Key, scales="free") +
   xlab(expression(sigma)) +
-  ylab("")
+  ylab("")+
+  ggtitle("(a)")
 
 
 ggsave(filename = sprintf("%s/latent1.pdf", fig.dir), plot = pp,
@@ -241,25 +262,28 @@ pp <- results  %>%
   pivot_longer(cols=c("FDR", "Power"), names_to='Key', values_to='Value') %>%
   mutate(Key = factor(Key, key.values, key.labels))  %>%
   ggplot(aes(x=Mean, y=Value, color=Method, shape=Method)) +
-  geom_point(alpha=0.75) +
-  geom_line() +
+  geom_point(alpha=pt_alpha, size=pt_size) +
+  geom_line(size=l_size, alpha=l_alpha) +
   geom_hline(data=df.nominal, aes(yintercept=Value)) +
   # sets the axis limit
   geom_point(data=df.ghost, aes(x=3.0,y=0.1), alpha=0) +
   geom_point(data=df.ghost, aes(x=4.0,y=0), alpha=0) +
   scale_color_manual(values=color.scale) +
   scale_shape_manual(values=shape.scale) +
-  theme_light()+
+  theme_bw()+
   theme(
     strip.text = element_text(size = font_size, color = "black"),
     axis.title = element_text(size = font_size),
     axis.text = element_text(size = axis_size),
     legend.text = element_text(size = legend_size),
-    legend.title = element_text(size = legend_size)
+    legend.title = element_text(size = legend_size),
+    plot.title = element_text(hjust = -0.11, vjust = -4, size=font_size),
+    
   )+
   facet_wrap(.~Key, scales="free") +
   xlab(expression(mu)) +
-  ylab("")
+  ylab("")+
+  ggtitle("(b)")
 
 
 ggsave(filename = sprintf("%s/latent2.pdf", fig.dir), plot = pp,
@@ -271,39 +295,43 @@ ggsave(filename = sprintf("%s/latent2.pdf", fig.dir), plot = pp,
 #       Latent-Mult setting 1        #
 #######################################
 Method.values <- c("BH", "LASLA.OR","LASLA.DD","AVG")
-Method.labels <- c("BH", "LASLA.OR","LASLA.DD","AVG")
+Method.labels <- c("BH", "LASLA.OR","Mahalanobis","Average")
 color.scale <- c("orange", "#CC79A7", "#66CCFF", "#3366CC")
 
 
 plot.alpha <- 0.05
 df.nominal <- tibble(Key=c("FDR"), Value=plot.alpha) %>%
   mutate(Key = factor(Key, key.values, key.labels))    
-df.ghost <- tibble(Key=c("FDR","FDR"), Value=c(0.03,0.07), Method="LASLA.DD") %>%
+df.ghost <- tibble(Key=c("FDR","FDR"), Value=c(0.03,0.07), Method="LASLA.OR") %>%
   mutate(Method = factor(Method, Method.values, Method.labels)) %>%
   mutate(Key = factor(Key, key.values, key.labels))    
 pp <- results  %>%
   pivot_longer(cols=c("FDR", "Power"), names_to='Key', values_to='Value') %>%
   mutate(Key = factor(Key, key.values, key.labels))  %>%
   ggplot(aes(x=Sigma, y=Value, color=Method, shape=Method)) +
-  geom_point(alpha=0.75) +
-  geom_line() +
+  geom_point(alpha=pt_alpha, size=pt_size) +
+  geom_line(size=l_size, alpha=l_alpha) +
   geom_hline(data=df.nominal, aes(yintercept=Value)) +
   # sets the axis limit
   geom_point(data=df.ghost, aes(x=0.5,y=0.1), alpha=0) +
   geom_point(data=df.ghost, aes(x=2.0,y=0), alpha=0) +
-  scale_color_manual(values=color.scale) +
-  scale_shape_manual(values=shape.scale) +
-  theme_light()+
+  scale_color_manual(values=color.scale, labels=Method.labels) +
+  scale_shape_manual(values=shape.scale, labels=Method.labels) +
+  theme_bw()+
   theme(
     strip.text = element_text(size = font_size, color = "black"),
     axis.title = element_text(size = font_size),
     axis.text = element_text(size = axis_size),
     legend.text = element_text(size = legend_size),
-    legend.title = element_text(size = legend_size)
+    legend.title = element_text(size = legend_size),
+    plot.title = element_text(hjust = -0.11, vjust = -4, size=font_size),
+    
   )+
   facet_wrap(.~Key, scales="free") +
   xlab(expression(sigma)) +
-  ylab("")
+  ylab("")+
+  ggtitle("(a)")
+  pp
 
 ggsave(filename = sprintf("%s/latent_mult1.pdf", fig.dir), plot = pp,
        dpi = 300, device=NULL, width=plot_width, height=plot_height)
@@ -314,39 +342,42 @@ ggsave(filename = sprintf("%s/latent_mult1.pdf", fig.dir), plot = pp,
 #       Latent-Mult setting 2        #
 #######################################
 Method.values <- c("BH", "LASLA.OR","LASLA.DD","AVG")
-Method.labels <- c("BH", "LASLA.OR","LASLA.DD","AVG")
+Method.labels <- c("BH", "LASLA.OR","Mahalanobis","Average")
 color.scale <- c("orange", "#CC79A7", "#66CCFF", "#3366CC")
 
 
 plot.alpha <- 0.05
 df.nominal <- tibble(Key=c("FDR"), Value=plot.alpha) %>%
   mutate(Key = factor(Key, key.values, key.labels))    
-df.ghost <- tibble(Key=c("FDR","FDR"), Value=c(0.03,0.07), Method="LASLA.DD") %>%
+df.ghost <- tibble(Key=c("FDR","FDR"), Value=c(0.03,0.07), Method="LASLA.OR") %>%
   mutate(Method = factor(Method, Method.values, Method.labels)) %>%
   mutate(Key = factor(Key, key.values, key.labels))    
 pp <- results  %>%
   pivot_longer(cols=c("FDR", "Power"), names_to='Key', values_to='Value') %>%
   mutate(Key = factor(Key, key.values, key.labels))  %>%
   ggplot(aes(x=Sigma, y=Value, color=Method, shape=Method)) +
-  geom_point(alpha=0.75) +
-  geom_line() +
+  geom_point(alpha=pt_alpha, size=pt_size) +
+  geom_line(size=l_size, alpha=l_alpha) +
   geom_hline(data=df.nominal, aes(yintercept=Value)) +
   # sets the axis limit
   geom_point(data=df.ghost, aes(x=0.5,y=0.1), alpha=0) +
   geom_point(data=df.ghost, aes(x=2.0,y=0), alpha=0) +
-  scale_color_manual(values=color.scale) +
-  scale_shape_manual(values=shape.scale) +
-  theme_light()+
+  scale_color_manual(values=color.scale, labels=Method.labels) +
+  scale_shape_manual(values=shape.scale, labels=Method.labels) +
+  theme_bw()+
   theme(
     strip.text = element_text(size = font_size, color = "black"),
     axis.title = element_text(size = font_size),
     axis.text = element_text(size = axis_size),
     legend.text = element_text(size = legend_size),
-    legend.title = element_text(size = legend_size)
+    legend.title = element_text(size = legend_size),
+    plot.title = element_text(hjust = -0.11, vjust = -4, size=font_size),
+    
   )+
   facet_wrap(.~Key, scales="free") +
   xlab(expression(sigma)) +
-  ylab("")
+  ylab("")+
+  ggtitle("(b)")
 
 ggsave(filename = sprintf("%s/latent_mult2.pdf", fig.dir), plot = pp,
        dpi = 300, device=NULL, width=plot_width, height=plot_height)
@@ -371,25 +402,29 @@ pp <- results  %>%
   pivot_longer(cols=c("FDR", "Power"), names_to='Key', values_to='Value') %>%
   mutate(Key = factor(Key, key.values, key.labels))  %>%
   ggplot(aes(x=Sigma, y=Value, color=Method, shape=Method)) +
-  geom_point(alpha=0.75) +
-  geom_line() +
+  geom_point(alpha=pt_alpha, size=pt_size) +
+  geom_line(size=l_size, alpha=l_alpha) +
   geom_hline(data=df.nominal, aes(yintercept=Value)) +
   # sets the axis limit
   geom_point(data=df.ghost, aes(x=0.05,y=0.1), alpha=0) +
   geom_point(data=df.ghost, aes(x=0.1,y=0), alpha=0) +
   scale_color_manual(values=color.scale) +
   scale_shape_manual(values=shape.scale) +
-  theme_light()+
+  theme_bw()+
   theme(
     strip.text = element_text(size = font_size, color = "black"),
     axis.title = element_text(size = font_size),
     axis.text = element_text(size = axis_size),
     legend.text = element_text(size = legend_size),
-    legend.title = element_text(size = legend_size)
+    legend.title = element_text(size = legend_size),
+    plot.title = element_text(hjust = -0.11, vjust = -4, size=font_size),
+    
   )+
   facet_wrap(.~Key, scales="free") +
   xlab(expression(sigma)) +
-  ylab("")
+  ylab("")+
+  ggtitle("(a)")
+
 
 ggsave(filename = sprintf("%s/regression1.pdf", fig.dir), plot = pp,
        dpi = 300, device=NULL, width=plot_width, height=plot_height)
@@ -414,25 +449,29 @@ pp <- results  %>%
   pivot_longer(cols=c("FDR", "Power"), names_to='Key', values_to='Value') %>%
   mutate(Key = factor(Key, key.values, key.labels))  %>%
   ggplot(aes(x=Mean, y=Value, color=Method, shape=Method)) +
-  geom_point(alpha=0.75) +
-  geom_line() +
+  geom_point(alpha=pt_alpha, size=pt_size) +
+  geom_line(size=l_size, alpha=l_alpha) +
   geom_hline(data=df.nominal, aes(yintercept=Value)) +
   # sets the axis limit
   geom_point(data=df.ghost, aes(x=0.25,y=0.1), alpha=0) +
   geom_point(data=df.ghost, aes(x=0.35,y=0), alpha=0) +
   scale_color_manual(values=color.scale) +
   scale_shape_manual(values=shape.scale) +
-  theme_light()+
+  theme_bw()+
   theme(
     strip.text = element_text(size = font_size, color = "black"),
     axis.title = element_text(size = font_size),
     axis.text = element_text(size = axis_size),
     legend.text = element_text(size = legend_size),
-    legend.title = element_text(size = legend_size)
+    legend.title = element_text(size = legend_size),
+    plot.title = element_text(hjust = -0.11, vjust = -4, size=font_size),
+    
   )+
   facet_wrap(.~Key, scales="free") +
   xlab(expression(mu)) +
-  ylab("")
+  ylab("")+
+  ggtitle("(b)")
+
 
 ggsave(filename = sprintf("%s/regression2.pdf", fig.dir), plot = pp,
        dpi = 300, device=NULL, width=plot_width, height=plot_height)
